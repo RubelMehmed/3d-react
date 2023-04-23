5f05cef5c644ed3508cc44e95255ff1ee3bbb9ed
RubelMehmed <rubel.49@geography-juniv.edu.bd>
2023-04-23 16:10:30 +0600

    production version

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Center, Environment } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 
-const Canvas = () => {
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+import Shirt from './Shirt';
+
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      <CameraRig>
+        <Backdrop />
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
