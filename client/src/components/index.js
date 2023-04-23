9f0e3cc13213a12eac896f18e2ed0c46904c7738
RubelMehmed <rubel.49@geography-juniv.edu.bd>
2023-04-23 10:33:14 +0600

    now address

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,13 @@
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
 import CustomButton from "./CustomButton";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
 
-
-export { CustomButton };
+export {
+    CustomButton,
+    AIPicker,
+    ColorPicker,
+    FilePicker,
+    Tab,
+};
